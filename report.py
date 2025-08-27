a7a38d6e2b736356192814c4a6298d9edb313cef
Carlos Villegas <carlos.villegas1@ibm.com>
2025-08-27 17:34:57 -0400

    almost?

--- report.py
+++ report.py
@@ -14,6 +14,9 @@ orgname="carlos-villegas-sandbox"
 def getWorkspaceId(workspacedict):
     return workspacedict['id']
 
+def getWorkspaceName(workspacedict):
+    return workspacedict['attributes']['name']
+
 def getResources(stateversiondict):
     return stateversiondict['attributes']['resources']
 
@@ -35,7 +38,8 @@ def listWorkspaces(tfce, orgname, page = 0, prevdata = []):
     #print(len(data))
     #print(data)
     jdata=json.loads(data)
-    workspaces=list(map(getWorkspaceId, jdata["data"]))
+    #workspaces=list(map(getWorkspaceId, jdata["data"]))
+    workspaces=list(jdata["data"])
     #print(workspaces)
     next_page=jdata["meta"]["pagination"]["next-page"]
     #print(next_page)
@@ -59,14 +63,16 @@ def getCurrentStateVersion(tfce, orgname, wsid):
 
 workspaces=listWorkspaces(tfce, orgname)
 
-print(workspaces)
+#workspaceIds=list(map(getWorkspaceId, workspaces))
+#print(workspaceIds)
 
 def printResourceLines(orgname, wsid, resources):
     for res in resources:
         print(tfce+","+orgname+","+wsid+","+res['type']+"\n")
 
-for wsid in workspaces:
+for ws in workspaces:
+    wsid=getWorkspaceId(ws)
+    wsname=getWorkspaceName(ws)
     time.sleep(0.05)
-    printResourceLines(orgname, wsid, getCurrentStateVersion(tfce, orgname, wsid))
-
+    printResourceLines(orgname, wsname, getCurrentStateVersion(tfce, orgname, wsid))
 
